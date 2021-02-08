61529b905183c41bb28040141c645ce719d5964e
Dominik <68650553+patyk409@users.noreply.github.com>
2021-02-08 11:44:47 +0100

    weather-app

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $.get('https://danepubliczne.imgw.pl/api/data/synop/')
+        .done(data => {
+            let body = $('body');
+            let container = $('<div></div>');
+            $(container).attr('class', 'container');
+            $(body).append(container);
+
+            for (city of data) {
+                let box = $('<div></div>');
+                $(box).attr('class', 'weather-box');
+                $(container).append(box);
+                $(box).html(`<h2>${city.stacja}</h2><p>${city.temperatura} °C</p><p>${city.cisnienie} hPa</p>`);
+            };
+
+            const input = document.querySelector('input');
+            $(input).on("keyup", function () {
+                let value = $(this).val().toLowerCase();
+                $('.weather-box').filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+                });
+            });
+        })
+
+        .fail(error => {
+            console.error(error);
+        });
+});
